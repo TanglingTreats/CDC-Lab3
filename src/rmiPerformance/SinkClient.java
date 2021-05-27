c5dbe7d243f3cf405040ce080f117fb4e9b681a6
Edwin Tang <edwintang10@gmail.com>
2021-05-28 02:10:12 +0800

    Add SinkClient lookup.

--- src/rmiPerformance/SinkClient.java
+++ src/rmiPerformance/SinkClient.java
@@ -1,7 +1,43 @@
 package rmiPerformance;
 
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class SinkClient {
+	private static String path = "rmi://%s:%d/SinkService";
+	private static String server_host = "localhost";
+	private static int server_port = 1099;
+	
+	public SinkClient() {
+		
+	}
+
 	public static void main (String[] args) {
+
+		try
+		{
+			IntSink s = (IntSink) Naming.lookup(String.format(path, server_host, server_port));
+
+		}
+		catch (MalformedURLException murle) {
+			System.out.println();
+			System.out.println("MalformedURLException");
+			System.out.println(murle);
+		} catch (RemoteException re) {
+			System.out.println();
+			System.out.println("RemoteException");
+			System.out.println(re);
+		} catch (NotBoundException nbe) {
+			System.out.println();
+			System.out.println("NotBoundException");
+			System.out.println(nbe);
+		} catch (java.lang.ArithmeticException ae) {
+			System.out.println();
+			System.out.println("java.lang.ArithmeticException");
+			System.out.println(ae);
+		}
 		
 	}
 
