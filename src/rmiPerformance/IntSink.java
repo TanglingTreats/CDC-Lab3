d8a5fd49556e409dec011ab6c592fa0aa7889d73
Edwin Tang <edwintang10@gmail.com>
2021-05-28 02:11:55 +0800

    Add comments in IntSink interface

--- src/rmiPerformance/IntSink.java
+++ src/rmiPerformance/IntSink.java
@@ -4,6 +4,12 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface IntSink extends Remote {
+	/**
+	 * Single method with single int argument used to time RMI calls
+	 * 
+	 * @param localParam
+	 * @throws RemoteException
+	 */
 	public void ignore(int localParam) throws RemoteException;
 
 }
